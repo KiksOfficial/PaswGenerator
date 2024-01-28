ac261fa796be381b464698e83722fb68aaf9defb
KiksOfficial <151571662+KiksOfficial@users.noreply.github.com>
2024-01-28 16:15:00 +0200

    Add files via upload

---
+++ PswGnrt.py
@@ -0,0 +1,29 @@
+import secrets
+import string
+import mysql.connector
+
+def generate_pass(length=12):
+    characters = string.ascii_letters + string.punctuation + string.digits
+    psw = ''.join(secrets.choice(characters) for i in range (length))
+    return psw
+
+print(generate_pass())
+
+Website = input('Website name: ')
+user = input('Username: ')
+
+generated = generate_pass()
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='loseyourself',
+    database='mydb'
+)
+cursor = mydb.cursor()
+
+formula = 'INSERT INTO kasutajad (website, user_name, pass_word) VALUES (%s, %s, %s)'
+idk = (Website, user, generated)
+
+cursor.execute(formula, idk)
+mydb.commit()
